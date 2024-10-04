daabb5e2a12e401842e734cc00c43e3c0c6fcfa1
Rafig Pashayev <pashayevler@gmail.com>
2024-10-04 04:21:56 +0400

    add pg db

---
+++ configurations/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PG_USER=os.getenv("PG_USER")
+PG_PASSWORD=os.getenv("PG_PASSWORD")
+PG_DATABASE=os.getenv("PG_DATABASE")
+PG_HOST=os.getenv("PG_HOST")
+PG_PORT=os.getenv("PG_PORT")
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DATABASE}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
